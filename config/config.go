4ac62a66752205df8396e6f26f7e33a3859f5761
Fauzi Sofyan <fauzilax@gmail.com>
2023-01-12 11:55:00 +0700

    edit config

--- config/config.go
+++ config/config.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/spf13/viper"
 )
@@ -26,21 +28,51 @@ func InitConfig() *DBConfig {
 
 func ReadEnv() *DBConfig {
 	app := DBConfig{}
-	viper.SetConfigName("local")
-	viper.AddConfigPath(".")
-	viper.SetConfigType("env")
-	err := viper.ReadInConfig()
-	if err != nil {
-		log.Println("error read config : ", err.Error())
-		return nil
+	isRead := true
+
+	if val, found := os.LookupEnv("JWT_KEY"); found {
+		app.jwtKey = val
+		isRead = false
 	}
-	err = viper.Unmarshal(&app)
-	if err != nil {
-		log.Println("error parse config : ", err.Error())
-		return nil
+	if val, found := os.LookupEnv("DBUSER"); found {
+		app.DBUser = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBPASS"); found {
+		app.DBPass = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBHOST"); found {
+		app.DBHost = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBPORT"); found {
+		cnv, _ := strconv.Atoi(val)
+		app.DBPort = cnv
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBNAME"); found {
+		app.DBName = val
+		isRead = false
 	}
 
-	JWTKey = app.jwtKey //isikan variable global JWTKey melalui fungsi ReadEnv yang diambil dari file Local.env
+	if isRead {
+		viper.AddConfigPath(".")
+		viper.SetConfigName("local")
+		viper.SetConfigType("env")
 
+		err := viper.ReadInConfig()
+		if err != nil {
+			log.Println("error read config : ", err.Error())
+			return nil
+		}
+		err = viper.Unmarshal(&app)
+		if err != nil {
+			log.Println("error parse config : ", err.Error())
+			return nil
+		}
+	}
+
+	JWTKey = app.jwtKey
 	return &app
 }
